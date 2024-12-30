1879cbfebfcb06b1f64e3c18435fad1f578af740
syzenzzw <oisyzenx@gmail.com>
2024-12-30 11:24:40 -0300

    git commit

---
+++ BlogDoEltinho/Models/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogDoEltinho.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
